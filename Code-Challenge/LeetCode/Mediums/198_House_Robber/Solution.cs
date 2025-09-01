fc509f26a912e52e9b670aba3861bbabe48be1e8
reza-ya <rezaayaari@gmail.com>
2025-09-01 11:24:08 +0330

    feat: solve 198. House Robber

---
+++ Code-Challenge/LeetCode/Mediums/198_House_Robber/Solution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenge.LeetCode.Mediums._198_House_Robber
+{
+    public class Solution
+    {
+        public int[]? houseMaxRob;
+        public int Rob(int[] nums)
+        {
+            houseMaxRob = new int[nums.Length];
+            for (int i = 0; i < houseMaxRob.Length; i++)
+            {
+                houseMaxRob[i] = -1;
+            }
+            if (nums.Length == 1) return nums[0];
+            int robStartAtOne = DFSRob(ref nums, 0, nums[0]);
+            int robStartAtTwo = DFSRob(ref nums, 1, nums[1]);
+
+
+            return Math.Max(robStartAtOne, robStartAtTwo);
+        }
+
+
+        public int DFSRob(ref int[] houses, int currentIndex, int RobAmmount)
+        {
+            if (houseMaxRob[currentIndex] >= RobAmmount)
+            {
+                return RobAmmount;
+            }
+            else
+            {
+                houseMaxRob[currentIndex] = RobAmmount;
+            }
+            int robStartAtOne = RobAmmount;
+            int robStartAtTwo = RobAmmount;
+            if (currentIndex + 2 <= houses.Length - 1)
+            {
+                robStartAtOne = DFSRob(ref houses, currentIndex + 2, houses[currentIndex + 2] + RobAmmount);
+            }
+            if (currentIndex + 3 <= houses.Length - 1)
+            {
+                robStartAtTwo = DFSRob(ref houses, currentIndex + 3, houses[currentIndex + 3] + RobAmmount);
+            }
+            
+            return Math.Max(robStartAtOne, robStartAtTwo);
+        }
+    }
+
+}
--- Code-Challenge/Program.cs
+++ Code-Challenge/Program.cs
@@ -1,8 +1,7 @@
 ﻿using Code_Challenge.LeetCode;
-using Code_Challenge.LeetCode.Mediums._36_Valid_Sudoku;
+using Code_Challenge.LeetCode.Mediums._198_House_Robber;
 
 using System.Collections;
-using System.Runtime.CompilerServices;
 
 namespace Code_Challenge
 {
@@ -10,54 +9,16 @@ namespace Code_Challenge
     {
         static void Main(string[] args)
         {
-            var obj = new object();
 
-
-            var hashTable = new Hashtable(9);
-            
 
             var solution = new Solution();
-            //var sudou = new char[9][] {
-            //    new [] {'8','3','.','.','7','.','.','.','.'},
-            //    new [] {'6','.','.','1','9','5','.','.','.'},
-            //    new [] {'.','9','8','.','.','.','.','6','.'},
-            //    new [] {'8','.','.','.','6','.','.','.','3'},
-            //    new [] {'4','.','.','8','.','3','.','.','1'},
-            //    new [] {'7','.','.','.','2','.','.','.','6'},
-            //    new [] {'.','6','.','.','.','.','2','8','.'},
-            //    new [] {'.','.','.','4','1','9','.','.','5'},
-            //    new [] {'.','.','.','.','8','.','.','7','9'}
-            //    };
-                var sudou = new char[9][] {
-                ['.', '.', '.', '.', '5', '.', '.', '1', '.'],
-                ['.', '4', '.', '3', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '3', '.', '.', '1'],
-                ['8', '.', '.', '.', '.', '.', '.', '2', '.'],
-                ['.', '.', '2', '.', '7', '.', '.', '.', '.'],
-                ['.', '1', '5', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '2', '.', '.', '.'],
-                ['.', '2', '.', '9', '.', '.', '.', '.', '.'],
-                ['.', '.', '4', '.', '.', '.', '.', '.', '.']
-                };
 
+            //var houses = new int[] { 1, 2, 3, 1 };
+            var houses = new int[] { 0, 0, 0, 1 };
 
-
-            var result = solution.IsValidSudoku(sudou);
+            var result = solution.Rob(houses);
 
             Console.WriteLine($"result: {result}");
-        }
-    }
-
-    public class HashProvider : IEqualityComparer
-    {
-        public new bool Equals(object? x, object? y)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int GetHashCode(object obj)
-        {
-            throw new NotImplementedException();
         }
     }
 }
