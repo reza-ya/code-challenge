9cdce066afc244a0750d5937eea587104f9ae1f5
reza-ya <rezaayaari@gmail.com>
2025-12-07 09:41:22 +0330

    fix: fix

--- Code-Challenge/LeetCode/Mediums/1884_Egg_Drop_With_2_Eggs_and_N_Floors/1884_Egg_Drop_With_2_Eggs_and_N_Floors.cs
+++ Code-Challenge/LeetCode/Mediums/1884_Egg_Drop_With_2_Eggs_and_N_Floors/1884_Egg_Drop_With_2_Eggs_and_N_Floors.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Code_Challenge.LeetCode.Mediums._1884_Egg_Drop_With_2_Eggs_and_N_Floors
+namespace Code_Challenge.LeetCode.Mediums
 {
-    public class Solution
+    public class Soluction
     {
         public int TwoEggDrop(int n)
         {
